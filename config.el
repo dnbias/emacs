266d6a8c2244c76019b2e24dca47b653fa7b4335
Daniel Biasiotto <daniel.biasiotto@edu.unito.it>
2022-11-30 15:55:15 +0100

    useless bit

--- config.el
+++ config.el
@@ -1,6 +1,4 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
 (setq user-full-name "Daniel Biasiotto"
        user-mail-address "mail@dnbias.dev")
 
