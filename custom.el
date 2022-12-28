bec725c7caf29203fc59ea18836b89b9caec9956
dnbias <daniel.biasiotto@edu.unito.it>
2022-12-28 19:09:03 +0100

    custom

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
  '(custom-safe-themes
    '("c2aeb1bd4aa80f1e4f95746bda040aafb78b1808de07d340007ba898efa484f5" "84b14a0a41bb2728568d40c545280dbe7d6891221e7fbe7c2b1c54a3f5959289" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" "850bb46cc41d8a28669f78b98db04a46053eca663db71a001b40288a9b36796c" "3d47380bf5aa650e7b8e049e7ae54cdada54d0637e7bac39e4cc6afb44e8463b" "a9a67b318b7417adbedaab02f05fa679973e9718d9d26075c6235b1f0db703c8" default))
  '(warning-suppress-types
@@ -17,8 +18,13 @@
  '(mode-line ((t (:family "SF Display" :height 1.0))))
  '(mode-line-inactive ((t (:family "SF Display" :height 1.0))))
  '(org-block ((t (:background nil))))
- '(org-document-title ((t (:family "Roboto" :height 150 :weight semibold))))
- '(org-level-1 ((t (:height 1.3 :weight bold :slant normal))))
- '(org-level-2 ((t (:height 1.2 :weight semibold :slant normal))))
- '(org-level-3 ((t (:height 1.1 :weight regular :slant normal))))
+ '(org-document-title ((t (:inherit default :weight bold :foreground "unspecified-fg" :font "SF Pro" :height 2.0 :underline nil))))
+ '(org-level-1 ((t (:inherit default :weight bold :foreground "unspecified-fg" :font "SF Pro" :height 1.75))))
+ '(org-level-2 ((t (:inherit default :weight bold :foreground "unspecified-fg" :font "SF Pro" :height 1.5))))
+ '(org-level-3 ((t (:inherit default :weight bold :foreground "unspecified-fg" :font "SF Pro" :height 1.25))))
+ '(org-level-4 ((t (:inherit default :weight bold :foreground "unspecified-fg" :font "SF Pro" :height 1.1))))
+ '(org-level-5 ((t (:inherit default :weight bold :foreground "unspecified-fg" :font "SF Pro"))))
+ '(org-level-6 ((t (:inherit default :weight bold :foreground "unspecified-fg" :font "SF Pro"))))
+ '(org-level-7 ((t (:inherit default :weight bold :foreground "unspecified-fg" :font "SF Pro"))))
+ '(org-level-8 ((t (:inherit default :weight bold :foreground "unspecified-fg" :font "SF Pro"))))
  '(org-quote ((t (:background nil :font "Adobe Garamond Pro" :slant normal)))))
